152699d181063e1a137efe5046eb7219a4eda6b6
zhpinkman <zhivarsourati@gmail.com>
2019-11-07 22:57:02 +0330

    first system call implemented but without passing arguments in registers

--- zhi.c
+++ zhi.c
@@ -5,6 +5,11 @@
 
 int main(int argc, char *argv[])
 {
-    write(1, "zhivar\n", 8);
+    if (argc != 2)
+    {
+        write(1, "not enough arguments\n", 22);
+        exit();
+    }
+    printf(1, "number of digits:  %d \n", count_num_of_digits(atoi(argv[1])));
     exit();
 }
